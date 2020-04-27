6c9db5a83d980fb38c5ac8bc91825214f931db7f
Vivek <vmpk2001@gmail.com>
2020-04-27 19:22:39 +0530

    Django second version

--- SMC/smc_app/views.py
+++ SMC/smc_app/views.py
@@ -6,8 +6,8 @@ def index(request):
     return render(request,'smc_app/index.html')
 
 def majju(request):
-    chatdata = ChatData.objects.order_by('-sentat')[:100][::-1]
-    chatdata_dict = {'chatdata':chatdata}
+    # chatdata = ChatData.objects.order_by('-sentat')[:100][::-1]
+    # chatdata_dict = {'chatdata':chatdata}
 
     if request.method == 'POST':
         textdata = request.POST.get("Textinput")
@@ -19,10 +19,26 @@ def majju(request):
     else:
         print('Error saving data')
 
-
+    chatdata = ChatData.objects.order_by('-sentat')[:100][::-1]
+    chatdata_dict = {'chatdata':chatdata}
     return render(request,'smc_app/majju.html',context=chatdata_dict)
 
 
 
 def sallu(request):
-    return render(request,'smc_app/sallu.html')
+    # chatdata = ChatData.objects.order_by('-sentat')[:100][::-1]
+    # chatdata_dict = {'chatdata':chatdata}
+
+    if request.method == 'POST':
+        textdata = request.POST.get("Textinput")
+        chattext = ChatData(name='S',text=textdata)
+        if len(textdata) > 0:
+            chattext.save()
+        # print('data saved in chat database for majju')
+
+    else:
+        pass
+
+    chatdata = ChatData.objects.order_by('-sentat')[:100][::-1]
+    chatdata_dict = {'chatdata':chatdata}
+    return render(request,'smc_app/sallu.html',context=chatdata_dict)
