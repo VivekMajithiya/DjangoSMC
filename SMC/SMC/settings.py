4b6b861df3624df3e3c3f89a90e5687cb3705eb9
VivekMajithiya <52253560+VivekMajithiya@users.noreply.github.com>
2020-05-04 01:54:04 +0530

    Update settings.py

--- SMC/SMC/settings.py
+++ SMC/SMC/settings.py
@@ -145,5 +145,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'pythonworldiscool@gmail.com'
+EMAIL_HOST_USER = ''
 EMAIL_HOST_PASSWORD = ''
